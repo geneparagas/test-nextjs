fd187324bfbfddfe952daaa030fda085ff13f5bf
geneparagas <gene.paragas0408@gmail.com>
2025-07-12 11:55:14 +0800

    simple add function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,96 @@
+import React, { useState } from 'react';
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const inter = Inter({ subsets: ['latin'] })
+interface Recipe {
+    name: string;
+    title: string;
+}
+
 
 export default function Home() {
+  const [view, setView] = useState('list');
+  const [recipeItems, setRecipeItems] = useState(() => {
+    return [
+      {
+        name: 'Gene Paragas',
+        title: 'adobong manok'
+      }
+    ]
+  });
+
+  const addRecipe = (newRecipe: Recipe) => {
+
+    setRecipeItems((prevRecipes: Recipe[]) => [...prevRecipes, {...newRecipe}]);
+    setView('list');
+  }
+
+  const AddRecipeForm: React.FC<{onSubmit: (recipe: Recipe) => void;}>  = ({onSubmit}) => {
+
+    const [formData, setFormData] = useState({
+      name: '',
+      title: ''
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { id, value } = e.target;
+        setFormData({ ...formData, [id]: value });
+    };
+
+    const handleSubmit = (e: { preventDefault: () => void; }) => {
+      e.preventDefault();
+
+      onSubmit(formData);
+    }
+
+    return (
+      <div className='container-fluid'>
+          <div className='row justify-content-center'>
+            <div className='col bg-white shadow-md p-3'>
+              <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label">Your Name</label>
+                    <input
+                        type="text"
+                        className='form-control'
+                        id="name"
+                        placeholder="Enter your name"
+                        value={formData.name}
+                        onChange={(e) => handleChange(e)}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label">Title</label>
+                    <input
+                        type="text"
+                        className='form-control'
+                        id="title"
+                        placeholder="Enter your title"
+                        value={formData.title}
+                        onChange={(e) => handleChange(e)}
+                    />
+                </div>
+                <div className='mb-3'>
+                  <button type="submit" className="btn btn-primary">Add</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+    )
+  }
+
   return (
     <>
       <Head>
-        <header className="d-flex flex-wrap justify-content-center py-3 border-bottom">Header </header>
+        <header className="d-flex flex-wrap justify-content-center py-3 border-bottom">
+          Header 
+        </header>
       </Head>
       <main>
-        <div className='container-fluid'>
+        {view === 'list' && <div className='container-fluid'>
           <div className='row'>
             <div className='col-3 p-3'>
               <div className='bg-white shadow-md p-3'>
@@ -24,11 +100,20 @@ export default function Home() {
             <div className='col-9 p-3'>
               <div className='bg-white shadow-md p-3'>
                 items
+                <div className='row'>
+                  {recipeItems.map(rec => (
+                    <div className='border mb-1'>{rec.title} - {rec.name}</div>
+                  ))}
+                </div>
               </div>
             </div> 
           </div>
-
-        </div>
+          <div className='p-3'>
+            <button type="button" className="btn btn-primary" onClick={() => setView('add')}>Add</button>
+          </div>
+        </div>}
+        
+        {view === 'add' && <AddRecipeForm onSubmit={addRecipe} />}
       </main>
     </>
   )
